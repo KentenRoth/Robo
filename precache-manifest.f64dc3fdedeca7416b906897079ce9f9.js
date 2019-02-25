0b800b1c58e549aea091673d57b5f7561f4c4891
KentenRoth <kentencroth@yahoo.com>
2019-02-25 14:35:08 -0600

    Updates

---
+++ precache-manifest.f64dc3fdedeca7416b906897079ce9f9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Robo/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "9c954a36115ed3920e06",
+    "url": "/Robo/static/js/runtime~main.9c954a36.js"
+  },
+  {
+    "revision": "56e2a32581430e1fa5ea",
+    "url": "/Robo/static/js/main.56e2a325.chunk.js"
+  },
+  {
+    "revision": "b4fffd5cf70b4e9478c7",
+    "url": "/Robo/static/js/2.b4fffd5c.chunk.js"
+  },
+  {
+    "revision": "56e2a32581430e1fa5ea",
+    "url": "/Robo/static/css/main.04a6a0b0.chunk.css"
+  },
+  {
+    "revision": "b4fffd5cf70b4e9478c7",
+    "url": "/Robo/static/css/2.b5dc598e.chunk.css"
+  },
+  {
+    "revision": "2f2a2974e3dfe744d85532dba8709c0c",
+    "url": "/Robo/index.html"
+  }
+];
